959d0efe8475c1e3a55a3cc1c9ac08076af77b4b
weslai <we8403964@gmail.com>
2023-07-31 12:06:59 +0200

    [MOD] add fid for real ms

--- stylegan3/metrics/frechet_inception_distance.py
+++ stylegan3/metrics/frechet_inception_distance.py
@@ -14,6 +14,7 @@ https://github.com/bioinf-jku/TTUR/blob/master/fid.py"""
 import numpy as np
 import scipy.linalg
 from . import metric_utils
+from . import metric_utils_real_ms
 
 #----------------------------------------------------------------------------
 
@@ -39,3 +40,24 @@ def compute_fid(opts, max_real, num_gen):
     return float(fid)
 
 #----------------------------------------------------------------------------
+
+def compute_fid_real_ms(opts, max_real, num_gen):
+    # Direct TorchScript translation of http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
+    detector_url = 'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/metrics/inception-2015-12-05.pkl'
+    detector_kwargs = dict(return_features=True) # Return raw features before the softmax layer.
+
+    mu_real, sigma_real = metric_utils_real_ms.compute_feature_stats_for_dataset(
+        opts=opts, detector_url=detector_url, detector_kwargs=detector_kwargs,
+        rel_lo=0, rel_hi=0, capture_mean_cov=True, max_items=max_real).get_mean_cov()
+
+    mu_gen, sigma_gen = metric_utils_real_ms.compute_feature_stats_for_generator(
+        opts=opts, detector_url=detector_url, detector_kwargs=detector_kwargs,
+        rel_lo=0, rel_hi=1, capture_mean_cov=True, max_items=num_gen).get_mean_cov()
+
+    if opts.rank != 0:
+        return float('nan')
+
+    m = np.square(mu_gen - mu_real).sum()
+    s, _ = scipy.linalg.sqrtm(np.dot(sigma_gen, sigma_real), disp=False) # pylint: disable=no-member
+    fid = np.real(m + np.trace(sigma_gen + sigma_real - s * 2))
+    return float(fid)
